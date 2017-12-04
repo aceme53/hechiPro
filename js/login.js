944e1d96fa09cd04d518e96d36b3ef8e3658dea3
aceme53 <284420440@qq.com>
2017-12-04 14:47:43 +0800

    登录提交跳转

---
+++ js/login.js
@@ -0,0 +1,9 @@
+/**
+ * Created by sumi_EC on 2017/12/04.
+ */
+var login = function () {
+    var name = $('[name="username"]').val();
+    var password = $('[name="password"]').val();
+    if (!name || !password) return;
+    location.href = location.href.replace('login.html', 'HTML/home.html');
+};
