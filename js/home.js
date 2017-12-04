a7bc8c198d8ee6477e18bddd8c80af6afd42a134
aceme53 <284420440@qq.com>
2017-12-04 17:52:05 +0800

    主页部分提交

---
+++ js/home.js
@@ -0,0 +1,14 @@
+/**
+ * Created by sumi_EC on 2017/12/04.
+ */
+$(function () {
+    /**
+     * 顶部日期获取
+     */
+    var now = new Date();
+    var mouth = now.getMonth() + 1;
+    var date = now.getDate();
+    var day = now.getDay();
+    var dayArr = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"];
+    $('#calendarClock1').text(mouth + "月" + date + "日   " + dayArr[day]);
+});
